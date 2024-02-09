0efccd5933d8ce494703fe84e95d4da1770e885c
juwamardott <juwamardood@gmail.com>
2024-02-09 19:49:42 +0800

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        alura: ["Allura"],
+        beleza: ["Belleza"],
+        windsong: ["WindSong"],
+        alison: ["Allison"],
+      },
+      colors: {
+        kuning: ["#E3D4B5"],
+        biru: ["#6D8C9C"],
+      },
+    },
+  },
+  plugins: [],
+};
